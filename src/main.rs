bd0b1622bfa0310b989ef11cdaf1c2f9054ac934
Seremiti <sepiaoi@outlook.com>
2024-09-17 10:39:14 +0000

    add devcontainer.json

--- src/main.rs
+++ src/main.rs
@@ -111,7 +111,7 @@ fn reset_sigpipe() {
 /// The entrypoint.
 fn main() {
     pretty_env_logger::try_init_timed_custom_env("DFT_LOG")
-        .expect("The logger has not been previously initialized");
+        .expect("The logger has not been previously initialized12121");
     reset_sigpipe();
 
     match options::parse_args() {
